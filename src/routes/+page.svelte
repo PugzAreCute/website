c759306e74e175a094ee57597f96246c106b4ea6
PugzAreCute <me@pugzarecute.com>
2023-10-26 12:11:36 +0530

    content: add some stuff about me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,19 @@
 </svelte:head>
 
 <h1>PugzAreCute</h1>
+<img class="profile_photo" src="https://avatars.githubusercontent.com/u/62641095" alt="PugzAreCute">
+<p>Hello, I am a pug!
+    <br>
+    I do stuff in C or Java sometimes.
+    <br>
+    Open for commissions. Use the contact page
+</p>
+<style>
+    .profile_photo{
+        border-radius: 1em;
+        width: 10%;
+    }
+    p{
+        font-size: large;
+    }
+</style>
