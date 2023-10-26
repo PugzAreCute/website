a5dd7fd6fa63ca2709c70eb3475a261894795d3a
PugzAreCute <me@pugzarecute.com>
2023-10-26 22:38:53 +0530

    oops

--- runit.sh
+++ runit.sh
@@ -4,7 +4,7 @@ pkill node
 cd production_out
 while [ true ]; do
     echo "(Re)Starting server..."
-    BUILD_ID=JENKINS_NO_KILL node build
+    node build
     for i in 3 2 1; do
         printf 'Server restarting in %s\n' "${i}"
         sleep 1
