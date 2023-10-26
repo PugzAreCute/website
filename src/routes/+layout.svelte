fa74a21397db2be616b6575d1bea464dceea0030
PugzAreCute <me@pugzarecute.com>
2023-10-26 10:41:25 +0530

    feat: base files!

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '$lib/Navbar.svelte';
+	import Footer from '$lib/Footer.svelte';
+</script>
+
+
+<div>
+	<Navbar />
+
+	<main>
+		<slot />
+	</main>
+	<Footer />
+
+</div>
+
+
+<style>
+	div {
+		min-height: 100vh;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+</style>
