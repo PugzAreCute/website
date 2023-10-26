2e8de5937aff37bb37e1ee46a16a312dcb5edd85
PugzAreCute <me@pugzarecute.com>
2023-10-26 10:40:35 +0530

    feat: footer and navbar component

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,15 @@
+<footer>
+	<p> Revision GIT_REVISION </p>
+</footer>
+
+<style>
+footer{
+	border-top: 5px dashed var(--border);
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-top: auto;
+}
+</style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,40 @@
+	<nav>
+		<span>PugzAreCute</span>
+		<div>
+			<a href="/">CI server</a>
+			<a href="/contact">Contact</a>
+			<a href="uptime.pugzarecute.com">Server Uptime</a>
+		</div>
+	</nav>
+
+<style>
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		border-bottom: 5px dashed var(--border);
+		flex-wrap: wrap;
+		width: 100%;
+	}
+	div{
+		display: flex;
+		align-items: end;
+		justify-content: space-between;
+	}
+
+	span {
+		font-weight: 100;
+		font-size: 3em;
+		text-decoration: none;
+		padding-bottom: 10px;
+	}
+
+	a {
+		color: var(--text);
+		text-decoration: none;
+		background-color: var(--accent-pink);
+		padding: 5px;
+		border-radius: var(--border-radius);
+		border: 2px dashed var(--border);
+	}
+</style>
