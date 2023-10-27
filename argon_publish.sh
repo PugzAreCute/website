5e3e6d124834f3723b66d10e59116c6f9ba04f7f
PugzAreCute <me@pugzarecute.com>
2023-10-27 11:05:36 +0530

    Wrap commit in ""

--- argon_publish.sh
+++ argon_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOGFILE="/tmp/runit_logs_$(date +%Y.%m.%d-%H.%M.%S.log)"
-echo "PUBLIC_GIT_COMMIT=$GIT_COMMIT" > .env
+echo "PUBLIC_GIT_COMMIT=\"$GIT_COMMIT\"" > .env
 echo "Using logfile $LOGFILE after fork"
 echo "Run NPM install"
 npm install --unsafe-perm=true --allow-root
